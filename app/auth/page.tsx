f5f7dbde7a0354f7e3b3e9472bfc86599bf80ea7
Muhammad Faisal <muh.faisal572@gmail.com>
2025-10-27 22:51:33 +0700

    feat: slicing register auth

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -29,6 +29,17 @@ const AuthPage = () => {
     confirmPassword: "",
   });
 
+  // handler
+  const handleLogin = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("login", inputLogin);
+  };
+
+  const handleRegister = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log("register", inputRegister);
+  };
+
   return (
     <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-green-50 to-green-100 p-4">
       <div className="w-full max-w-md">
@@ -48,8 +59,8 @@ const AuthPage = () => {
             <TabsTrigger value="login">Login</TabsTrigger>
             <TabsTrigger value="register">Register</TabsTrigger>
           </TabsList>
-          {/* login  */}
 
+          {/* login  */}
           <TabsContent value="login">
             <Card>
               <CardHeader className="text-center">
@@ -61,7 +72,7 @@ const AuthPage = () => {
                 </CardDescription>
               </CardHeader>
               <CardContent>
-                <form action="" className="space-y-4">
+                <form onSubmit={handleLogin} className="space-y-4">
                   <div className="space-y-2">
                     <Label htmlFor="login-email">Email</Label>
                     <Input
@@ -111,6 +122,111 @@ const AuthPage = () => {
                       </>
                     ) : (
                       "Login"
+                    )}
+                  </Button>
+                </form>
+              </CardContent>
+            </Card>
+          </TabsContent>
+
+          {/* register */}
+          <TabsContent value="register">
+            <Card>
+              <CardHeader className="text-center">
+                <CardTitle className="text-xl text-gray-800">
+                  Create Your Account!
+                </CardTitle>
+                <CardDescription>Register to Start Chatting</CardDescription>
+              </CardHeader>
+              <CardContent>
+                <form onSubmit={handleRegister} className="space-y-4">
+                  <div className="space-y-2">
+                    <Label htmlFor="fullname-register">Fullname</Label>
+                    <Input
+                      id="fullname-register"
+                      type="text"
+                      placeholder="Fullname"
+                      value={inputRegister.fullname}
+                      onChange={(e) =>
+                        setInputRegister({
+                          ...inputRegister,
+                          fullname: e.target.value,
+                        })
+                      }
+                      required
+                      disabled={loading}
+                    />
+                  </div>
+                  <div className="space-y-2">
+                    <Label htmlFor="email-register">Email</Label>
+                    <Input
+                      id="email-register"
+                      type="email"
+                      placeholder="Email"
+                      value={inputRegister.email}
+                      onChange={(e) =>
+                        setInputRegister({
+                          ...inputRegister,
+                          email: e.target.value,
+                        })
+                      }
+                      required
+                      disabled={loading}
+                    />
+                  </div>
+                  <div className="space-y-2">
+                    <Label htmlFor="password-register">Password</Label>
+                    <Input
+                      id="password-register"
+                      type="password"
+                      placeholder="Password"
+                      value={inputRegister.password}
+                      onChange={(e) =>
+                        setInputRegister({
+                          ...inputRegister,
+                          password: e.target.value,
+                        })
+                      }
+                      required
+                      disabled={loading}
+                    />
+                  </div>
+                  <div className="space-y-2">
+                    <Label htmlFor="confirm-password-register">
+                      Confirm Password
+                    </Label>
+                    <Input
+                      id="confirm-password-register"
+                      type="password"
+                      placeholder="Password"
+                      value={inputRegister.confirmPassword}
+                      onChange={(e) =>
+                        setInputRegister({
+                          ...inputRegister,
+                          confirmPassword: e.target.value,
+                        })
+                      }
+                      required
+                      disabled={loading}
+                    />
+                  </div>
+                  {error && (
+                    <div className="bg-red-50 border border-red-200 text-red-700 px-4 py-3 rounded-md text-sm">
+                      {error}
+                    </div>
+                  )}
+
+                  <Button
+                    type="submit"
+                    className="w-full bg-green-500 hover:bg-green-800 hover:cursor-pointer"
+                  >
+                    {loading ? (
+                      <>
+                        <Loader2 className="mr-2 size-4 animate-spin" />
+                        Loading...
+                      </>
+                    ) : (
+                      "Register"
                     )}
                   </Button>
                 </form>
